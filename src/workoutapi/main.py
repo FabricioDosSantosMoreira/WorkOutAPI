89263cc6b9d1764713ecbea0b8883a0946f9708b
Fabrício Dos Santos Moreira <fabriciodossantosmoreira230305@gmail.com>
2024-06-12 16:02:29 -0300

    FIX: main

--- src/workoutapi/main.py
+++ src/workoutapi/main.py
@@ -7,7 +7,7 @@ def read_root():
     return {"Hello": "World"}
 
 
-if __name__ == "main":
+if __name__ == '__main__':
     import uvicorn
 
     uvicorn.run('main:app', host='0.0.0.0', port=8000, log_level='info', reload=True)
