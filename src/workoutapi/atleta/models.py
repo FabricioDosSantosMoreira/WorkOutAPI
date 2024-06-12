5b390a94e755e87414b810a149332c003904e63b
Fabrício Dos Santos Moreira <fabriciodossantosmoreira230305@gmail.com>
2024-06-12 15:49:47 -0300

    UPDATE: atleta e categoria models

--- src/workoutapi/atleta/models.py
+++ src/workoutapi/atleta/models.py
@@ -1,6 +1,7 @@
 from datetime import datetime
-from sqlalchemy import DateTime, Integer, String, Float
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import DateTime, ForeignKey, Integer, String, Float
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from workoutapi.categoria.models import CategoriaModel
 from workoutapi.contrib.models import BaseModel
 
 class AtletaModel(BaseModel):
@@ -14,4 +15,6 @@ class AtletaModel(BaseModel):
     altura: Mapped[float] = mapped_column(Float, nullable=False)
     sexo: Mapped[str] = mapped_column(String(1), nullable=False)
     created_at: Mapped[datetime] = mapped_column(DateTime, nullable=False)
-    
+
+    categoria: Mapped['CategoriaModel'] = relationship(back_populates='atleta')
+    categoria_id = Mapped[int] = mapped_column(ForeignKey('categorias.pk_id'))
--- src/workoutapi/categoria/models.py
+++ src/workoutapi/categoria/models.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Integer, String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from workoutapi.atleta.models import AtletaModel
 from workoutapi.contrib.models import BaseModel
 
 
@@ -8,3 +9,5 @@ class CategoriaModel(BaseModel):
 
     pk_id: Mapped[int] = mapped_column(Integer, primary_key=True)
     nome: Mapped[str] = mapped_column(String(11), nullable=False)
+
+    atleta: Mapped['AtletaModel'] = relationship(back_populates='categoria')
