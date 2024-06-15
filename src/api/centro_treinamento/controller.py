e87913c5daa4b118f4b86fb18ed4d4bfa0ff4c16
Fabrício Dos Santos Moreira <fabriciodossantosmoreira230305@gmail.com>
2024-06-14 22:59:55 -0300

    UPDATE: WorkOutAPI

--- src/api/centro_treinamento/controller.py
+++ src/api/centro_treinamento/controller.py
@@ -1,8 +1,8 @@
+from typing import Any
 from uuid import uuid4
 
 from api.centro_treinamento.models import CentroTreinamentoModel
-from api.centro_treinamento.schemas import (CentroTreinamentoIn,
-                                            CentroTreinamentoOut)
+from api.centro_treinamento.schemas import CentroTreinamentoIn, CentroTreinamentoOut, CentroTreinamentoUpdate
 from api.contrib.dependencies import DataBaseDependency
 from fastapi import APIRouter, Body, HTTPException, status
 from fastapi_pagination import Page
@@ -14,78 +14,135 @@ from sqlalchemy.future import select
 router = APIRouter()
 
 
-@router.post(
-        path='/',
-        summary='Criar um novo Centro de treinmamento',
-        status_code=status.HTTP_201_CREATED,
-        response_model=CentroTreinamentoOut,
+@router.get(
+    path="/",
+    summary="Consultar todos os centros de treinamento",
+    status_code=status.HTTP_200_OK,
+    response_model=Page[CentroTreinamentoOut],
+)
+async def query(db_session: DataBaseDependency) -> Page[CentroTreinamentoOut]:
 
+    return await paginate(db_session, select(CentroTreinamentoModel))
+
+
+@router.get(
+    path="/{id}",
+    summary="Consultar um centro de treinamento por Id",
+    status_code=status.HTTP_200_OK,
+    response_model=CentroTreinamentoOut,
+)
+async def query(id: UUID4, db_session: DataBaseDependency) -> CentroTreinamentoOut:
+
+    query = select(CentroTreinamentoModel).filter_by(id=id)
+
+    result: CentroTreinamentoOut = (await db_session.execute(query)).scalars().first()
+
+    if not result:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Centro de treinamento não encontrado no Id: {id}",
+        )
+
+    return result
+
+
+@router.post(
+    path="/",
+    summary="Criar um novo Centro de treinmamento",
+    status_code=status.HTTP_201_CREATED,
+    response_model=CentroTreinamentoOut,
 )
 async def post(
     db_session: DataBaseDependency,
-    centro_treinamento_in: CentroTreinamentoIn = Body(...)
+    centro_treinamento_in: CentroTreinamentoIn = Body(...),
 ) -> CentroTreinamentoOut:
-    
+
     try:
-        centro_treinamento_out = CentroTreinamentoOut(id=uuid4(), **centro_treinamento_in.model_dump())
-        centro_treinamento_model = CentroTreinamentoModel(**centro_treinamento_out.model_dump())
-        
+        centro_treinamento_out = CentroTreinamentoOut(
+            id=uuid4(), **centro_treinamento_in.model_dump()
+        )
+        centro_treinamento_model = CentroTreinamentoModel(
+            **centro_treinamento_out.model_dump()
+        )
+
         db_session.add(centro_treinamento_model)
+
         await db_session.commit()
 
     except IntegrityError as e:
         raise HTTPException(
             status_code=status.HTTP_303_SEE_OTHER,
-            detail=f'Já existe um centro de treinamento com o nome: {centro_treinamento_in.nome}'
+            detail=f"Já existe um centro de treinamento com o nome: {centro_treinamento_in.nome}",
         )
-    
+
     except Exception as exc:
         raise HTTPException(
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail=f'Ocorreu um erro ao inserir os dados no banco'
+            detail=f"Ocorreu um erro ao inserir no banco de dados",
         )
-
 
     return centro_treinamento_out
 
 
-
-
-@router.get(
-        path='/',
-        summary='Consultar todas os centros de treinamento',
-        status_code=status.HTTP_200_OK,
-        response_model=Page[CentroTreinamentoOut],
-
+@router.patch(
+    path="/",
+    summary="Editar um centro de treinamento por Id",
+    status_code=status.HTTP_201_CREATED,
+    response_model=CentroTreinamentoOut,
 )
-async def query(db_session: DataBaseDependency) -> Page[CentroTreinamentoOut]:
+async def query(
+    id: UUID4, db_session: DataBaseDependency, 
+    centro_treinamento_up: CentroTreinamentoUpdate = Body(...)
+) -> CentroTreinamentoOut:
     
-    return await paginate(db_session, select(CentroTreinamentoModel))
+    query = select(CentroTreinamentoModel).filter_by(id=id)
 
-
-    
-    
-    #centros_treinamentos: list[CentroTreinamentoOut] = (await db_session.execute(select(CentroTreinamentoModel))).scalars().all()
+    result: CentroTreinamentoOut = (await db_session.execute(query)).scalars().first()
    
-    #return centros_treinamentos
+    if not result:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, 
+            detail=f'Centro de treinamento não encontrado no Id: {id}'
+        )
+
+    centro_treinamento_up = centro_treinamento_up.model_dump(exclude_unset=True)
+    for key, value in centro_treinamento_up.items():
+        setattr(result, key, value)
+
+    await db_session.commit()
+    await db_session.refresh(result)
+
+    return result
 
 
-
-
-@router.get(
-        path='/{id}',
-        summary='Consultar um centro de treinamento',
-        status_code=status.HTTP_200_OK,
-        response_model=CentroTreinamentoOut,
-
+@router.delete(
+    path='/{id}',
+    summary='Deletar um centro de treinamento por Id',
+    status_code=status.HTTP_204_NO_CONTENT,
 )
-async def query(id: UUID4, db_session: DataBaseDependency) -> CentroTreinamentoOut:
-    centros_treinamento: CentroTreinamentoOut = (await db_session.execute(select(CentroTreinamentoModel).filter_by(id=id))).scalars().first()
+async def query(
+    id: UUID4, 
+    db_session: DataBaseDependency
+) -> None:
+
+    query = select(CentroTreinamentoModel).filter_by(id=id)
+
+    result: CentroTreinamentoOut = (await db_session.execute(query)).scalars().first()
    
-    if not centros_treinamento:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Centro de treinamento não encontrado no id: {id}')
+    if not result:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, 
+            detail=f'Centro de treinamento não encontrado no Id: {id}')
 
+    try:
+        await db_session.delete(result)
+        await db_session.commit()
 
-    return centros_treinamento
+    except IntegrityError as e:
+        await db_session.rollback()
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail='Impossível deletar um centro de treinamento que está relacionado a um atleta',
+        )
 
-
+    return None
--- src/api/centro_treinamento/schemas.py
+++ src/api/centro_treinamento/schemas.py
@@ -4,7 +4,7 @@ from api.contrib.schemas import BaseSchema
 from pydantic import UUID4, Field
 
 
-class CentroTreinamentoIn(BaseSchema):
+class CentroTreinamento(BaseSchema):
 
     nome: Annotated[
         str,
@@ -32,6 +32,15 @@ class CentroTreinamentoIn(BaseSchema):
     ]
 
 
+class CentroTreinamentoIn(CentroTreinamento):
+    pass
+
+
+class CentroTreinamentoOut(CentroTreinamentoIn):
+
+    id: Annotated[UUID4, Field(description="Identificador do centro de treinamento")]
+
+
 class CentroTreinamentoAtleta(BaseSchema):
     nome: Annotated[
         str,
@@ -43,6 +52,29 @@ class CentroTreinamentoAtleta(BaseSchema):
     ]
 
 
-class CentroTreinamentoOut(CentroTreinamentoIn):
-    
-    id: Annotated[UUID4, Field(description="Identificador do centro de treinamento")]
+class CentroTreinamentoUpdate(BaseSchema):
+        
+    nome: Annotated[
+        str,
+        Field(
+            description="Nome do centro de treinamento",
+            example="CT Example",
+            max_length=20,
+        ),
+    ]
+    endereco: Annotated[
+        str,
+        Field(
+            description="Endereço do centro de treinamento",
+            example="Rua X, Q00",
+            max_length=60,
+        ),
+    ]
+    proprietario: Annotated[
+        str,
+        Field(
+            description="Proprietário do centro de treinamento",
+            example="Roberto",
+            max_length=30,
+        ),
+    ]
